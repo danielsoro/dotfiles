8f5f5c950ca9857c6fba43a16acde007864d67f6
Daniel Cunha <danielsoro@gmail.com>
2025-11-17 21:30:24 -0300

    Update flakes and fixing nix file

--- flake.nix
+++ flake.nix
@@ -143,7 +143,7 @@
               env = pkgs.buildEnv {
                 name = "system-applications";
                 paths = config.environment.systemPackages;
-                pathsToLink = "/Applications";
+                pathsToLink = ["/Applications"];
               };
             in
             pkgs.lib.mkForce ''
